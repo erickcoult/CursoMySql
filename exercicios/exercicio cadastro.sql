884636e0f36dd8a05e5650208988e175fa2af81c
Erick Coutinho <erickcoutinho08@hotmail.com>
2024-02-06 18:46:20 -0300

    Update exercicio cadastro.sql

--- exercicios/exercicio cadastro.sql
+++ exercicios/exercicio cadastro.sql
@@ -1,10 +1,4 @@
-create table pessoas (
-id int not null auto_increment,
-nome varchar(30) NOT NULL,
-nascimento date,
-sexo enum('M','F'),
-peso decimal (5,2),
-altura decimal(3,2),
-nascionalidade varchar(20) DEFAULT 'Brasil',
-primary key (id)
-) default charset = utf8; 
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nascionalidade)
+values
+('Godofredo', '1995-02-06', 'M', '78.5', '1.83', 'Brasil');
